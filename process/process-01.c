ad2f70e008412a7dc8fc87e348a8af7702ce218f
Shrikant Bobade <bobadeshrikant@gmail.com>
2017-04-09 06:29:06 +0530

    process-01.c: first process creation program
    Signed-off-by: Shrikant Bobade <bobadeshrikant@gmail.com>

---
+++ process/process-01.c
@@ -0,0 +1,31 @@
+/* file: process-01.c
+ *
+ * fork : system call to create child process
+ * check the return type: if greater than zero then it is child process,
+ * else parent process.
+ * 
+ */	
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+
+int main()
+{
+	int iret1;
+	printf("\n Process Creation Started...\n");
+	
+	system("ls -l");	
+	iret1 = fork();
+
+	if(iret1 == 0)
+	{
+		printf("\n Hey I am Child......PID: %d,,Parents PID:..%d.\n",getpid(),getppid());
+	}
+	else
+	{
+		printf("\n Hey I am Parent..PID= %d.. Parent PID: %d\n",getpid(),getppid());
+	}
+}	
